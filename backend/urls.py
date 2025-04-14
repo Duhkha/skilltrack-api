39fc4f18f7595b6f4f39c72b8d2f0165e2ba9f13
tarikdogic93 <tarikdogic93@gmail.com>
2025-04-14 14:21:07 +0200

    Fixed error from swagger

--- backend/urls.py
+++ backend/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework import permissions
+from rest_framework.permissions import AllowAny
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
@@ -10,7 +10,8 @@ schema_view = get_schema_view(
         default_version="v1",
     ),
     public=True,
-    permission_classes=(permissions.AllowAny,),
+    permission_classes=(AllowAny,),
+    authentication_classes=[],
 )
 
 urlpatterns = [
